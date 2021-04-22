09be0c59a9ea8ed701ff78e5099ce5de4af9c5e2
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-04-22 16:21:44 -0500

    Task 6 with MVP complete

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- 
+
  class Student extends Lambdasian{
      constructor(obj){
        super(obj);
@@ -206,9 +206,21 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor{
+     constructor(obj){
+       super(obj);
+       this.gradClassName = obj.gradClassName;
+       this.favInstructor = obj.favInstructor;
+     }
+
+     standUp(slackChannel){
+       return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+     }
+     debugsCode(studentObj, subject){
+       return `${this.name} debugs ${studentObj.name}'s code on ${subject}`;
+     }
  }
+
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
