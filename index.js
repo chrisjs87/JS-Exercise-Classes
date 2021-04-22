179eb60f9d2000e4817ce0ff8a06a3bf67fc735a
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-04-22 15:52:22 -0500

    Task 2 complete

--- index.js
+++ index.js
@@ -74,12 +74,53 @@ class Airplane {
       - Give cars ability to `.drive(distance)`. The distance driven:
           + Should cause the `odometer` to go up.
           + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
+
+
+
+
+
       - A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+
+    fill(gallons){
+      this.tank += gallons;
+    }
+    drive(distance){
+      // if(this.tank === 0){
+      //   return `I ran out of fuel at ${this.odometer} miles!`;
+      // }
+      // this.odometer += distance;
+      // this.tank -= distance/this.milesPerGallon;
+      if(this.tank - distance/this.milesPerGallon >= 0){
+
+        this.odometer += distance;
+
+        this.tank -= distance/this.milesPerGallon;
+
+      } 
+      else {
+
+        let distanceCanTravel;
+
+        distanceCanTravel = this.tank*this.milesPerGallon;
+
+        this.odometer += distanceCanTravel;
+
+        this.tank -= distanceCanTravel/this.milesPerGallon;
+
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
   }
   
   /*
